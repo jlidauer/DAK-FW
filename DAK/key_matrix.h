857496a1f85e207b33ca468b5c5a66b698297de2
jlidauer <jaakob.lidauer@gmail.com>
2018-03-08 20:20:33 +0200

    PCB files added.

--- DAK/key_matrix.h
+++ DAK/key_matrix.h
@@ -24,10 +24,11 @@
 //If the DEBUG_PRINT_STATES_ARRAY is uncommented, will the states array be printed to the serial monitor. 
 //This feature is useful to make sure that all switches are working properly. Use only for debugging.
 //To enable the serialport set in the IDE: Tools -> USB type: -> 'Serial + Keyboard + Mouse + Joystic'
+
 //#define DEBUG_PRINT_STATES_ARRAY
 
 //KEY MATRIX size:
-#define ROWS 9
+#define ROWS 11
 #define COLUMNS 14
 
 //OI pins:
@@ -35,38 +36,40 @@
 //syntax:
 //"#define [NAME] [pin number]"
 //ROWS:
-#define R1 21
-#define R2 20
-#define R3 19
-#define R4 18
-#define R5 17
+#define R1 22
+#define R2 21
+#define R3 20
+#define R4 19
+#define R5 18
 #define R6 16
-#define R7 15
-#define R8 14
-#define R9 12 
+#define R7 14
+#define R8 13
+#define R9 26 
+#define R10 15 
+#define R11 12 
 
 //COLUMNS:
-#define C1 11
-#define C2 10
-#define C3 9
-#define C4 8
-#define C5 1
-#define C6 7
-#define C7 2
-#define C8 6
-#define C9 22
-#define C10 5
-#define C11 4
-#define C12 3
-#define C13 13
+#define C1 9
+#define C2 8
+#define C3 7
+#define C4 6
+#define C5 5
+#define C6 4
+#define C7 3
+#define C8 2
+#define C9 1
+#define C10 0
+#define C11 24
+#define C12 25
+#define C13 17
 #define C14 23
 
 //Output pins for LEDs
-#define LED1 0  //CAPS LOCK
-#define LED2 24 //FN LOCK
+#define LED1 10  //CAPS LOCK
+#define LED2 11 //FN LOCK
 
 //Add here all the rows and the columns defined above, in order from the smallest to the biggest.
-const uint8_t ROW_PINS[] = {R1, R2, R3, R4, R5, R6, R7, R8, R9};
+const uint8_t ROW_PINS[] = {R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11};
 const uint8_t COLUMN_PINS[] = {C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14};
 
 #endif
